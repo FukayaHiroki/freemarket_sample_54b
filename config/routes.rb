4b325119bc0a00fc0cda649426097c48859cf08c
Osatsu-cat <moluca5460@gmail.com>
2019-07-09 16:48:10 +0900

    add routing & users/products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  root 'products#index'
+  resources :users, only: [:show] do
+    collection  do
+      get 'sign_up/resistration' => 'user#registration'
+      get 'sign_up/sms_confirmation' => 'user#sms'
+      get 'sign_up/sms_confirmation/sms' => 'users#sms_confirm'
+      get 'sign_up/deliver_adrerss' => 'users#adress'
+      get 'sign_up/card' => 'users#card'
+      get 'sign_up/done' => 'users#done'
+      get :logout
+    end
+    member do
+      get :identification, :mycard, :mycard_create, :profile 
+    end
+  end
+  resources :products, except: [:edit]
+  get 'products/buy/:id' => 'products#buy'
 end
