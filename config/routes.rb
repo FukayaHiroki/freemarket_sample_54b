413c847fc2cd732568bdad65ea724488b89e53b1
Tomoki <tech54tu@gmail.com>
2019-07-11 19:49:28 +0900

    仮置きのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,13 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :users, only: [:show] do
     collection  do
-      get 'sign_up/resistration' => 'users#registration'
+      get 'sign_up/registration' => 'users#registration'
       get 'sign_up/sms_confirmation' => 'users#sms'
       get 'sign_up/sms_confirmation/sms' => 'users#sms_confirm'
       get 'sign_up/deliver_adrerss' => 'users#adress'
       get 'sign_up/card' => 'users#card'
       get 'sign_up/done' => 'users#done'
       get :logout
-
-    # 仮置きのルーティング
-      get 'sign_up/' => 'users#signup'
-      get 'sign_in/' => 'users#login'
 
     end
     member do
@@ -29,4 +25,7 @@ Rails.application.routes.draw do
   get 'mycard_create' => 'users#mycard_create'
   get 'profile' => 'users#profile'
   get 'users' => 'users#show'
+  get 'sign_up' => 'users#signup'
+  get 'sign_in' => 'users#login'
+
 end
