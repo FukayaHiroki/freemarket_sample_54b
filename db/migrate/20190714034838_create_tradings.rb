48250efe54c219f9f97eed843616dd9807f466fb
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-07-14 13:25:41 +0900

    tradingテーブル、imageテーブルのマイグレーションファイルの作成

--- db/migrate/20190714034838_create_tradings.rb
+++ db/migrate/20190714034838_create_tradings.rb
@@ -2,7 +2,7 @@ class CreateTradings < ActiveRecord::Migration[5.2]
   def change
     create_table :tradings do |t|
       t.integer :status, null: false
-      t.references :products, foreign_key: true
+      t.references :product, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
     end
---
+++ db/migrate/20190714042333_add_image_to_products.rb
@@ -0,0 +1,5 @@
+class AddImageToProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :products, :image, foreign_key: true
+  end
+end
