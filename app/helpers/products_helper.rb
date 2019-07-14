c1b63cd83b8209d2e4d35a0b22293147a0f8ce48
FukayaHiroki <asitanonegif@gmail.com>
2019-07-14 17:31:28 +0900

    create itemChangeBox
    削除変更ボタンの作成と部分テンプレート化
    ヘルパーメソッドのバリデーション付けてあります

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+  def user_login
+    `.item-change-box
+       = link_to "#", class: "item-change-box__btn item-btn-edit" do
+         商品の編集
+       %p.text_center or
+       = link_to "#", class: "item-change-box__btn item-btn-gray" do
+         出品を一旦停止する
+       = link_to "#", class: "item-change-box__btn item-btn-gray" do
+         この商品を削除する`
+  end
 end
