7a8c9882fcdc54b297194d12c60b2b82f2421336
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-07-14 12:25:23 +0900

    create products table

--- db/migrate/20190713113535_create_products.rb
+++ db/migrate/20190713113535_create_products.rb
@@ -1,8 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string :name, null: false
+      t.text :detail, null:false
+      t.references :user, foreign_key: true
+      t.references :image, foreign_key: true
       t.timestamps
     end
+
+    add_index :products, :name
   end
 end
+
--- db/migrate/20190713115551_create_tradings.rb
+++ db/migrate/20190713115551_create_tradings.rb
@@ -1,7 +1,9 @@
 class CreateTradings < ActiveRecord::Migration[5.2]
   def change
     create_table :tradings do |t|
-
+      t.integer :status, null: false
+      t.references :products, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
