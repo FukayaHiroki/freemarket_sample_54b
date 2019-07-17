2707e4abd54b48eaa9dce84a33ce95a352d8a550
FukayaHiroki <asitanonegif@gmail.com>
2019-07-17 14:21:48 +0900

    tweak

--- db/migrate/20190716111238_remove_category_id_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryIdFromProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :products, :large_category_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_111238) do
+ActiveRecord::Schema.define(version: 2019_07_16_110756) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "url", null: false
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2019_07_16_111238) do
   end
 
   add_foreign_key "images", "products"
-  add_foreign_key "products", "large_categories"
   add_foreign_key "products", "users"
   add_foreign_key "tradings", "products"
   add_foreign_key "tradings", "users"
