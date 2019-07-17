1e1a1699d11436c9e8984e92615f3490862393bf
Tomoki <tech54tu@gmail.com>
2019-07-17 18:24:17 +0900

    クレジットカード登録実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery 
+//= require jquery_ujs 
 //= require activestorage
 //= require turbolinks
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
+
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,43 @@
 class CardsController < ApplicationController
-  protect_from_forgery except: :pay
+
   require "payjp"
-    # respond_to do |format|
-    #   format.html { redirect_to group_messages_path(@group)}
-    #   format.json
-    # end
+
+  def index
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      card = Card.find_by(user_id: current_user.id)
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card = customer.cards.retrieve(card.card_id) 
+    end
+  end
+
+  def new
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      redirect_to cards_path
+    end
+  end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create
+    card = customer.cards.create(card: params[:payjp_token])
+    @card = Card.create(user_id: current_user.id, customer_id: customer.id, card_id: card.id)
+    redirect_to cards_path
+  end
+
+  def delete
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to new_card_path
+  end
 end
