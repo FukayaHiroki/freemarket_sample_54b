639ed082afb924c24a636c3ac2264a77b901887b
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-07-20 20:59:35 +0900

    category js json.jbuilderの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,6 +58,11 @@ class ProductsController < ApplicationController
     @product.images.build
     @product.build_trading
     @product.build_large_category
+
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def create
@@ -84,6 +89,15 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  # カテゴリーjsのアクション
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   
   private
