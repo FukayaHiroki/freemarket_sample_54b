46eb528f8f7ca629849a3f6048c8a58a483776db
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-07-20 21:19:26 +0900

    一覧表示機能の実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  # process resize_to_fit: [400, 400]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+    process resize_to_fit: [300, 300]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
