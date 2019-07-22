73154c8fb61177e390cc3e49e71cff9374acc7bc
FukayaHiroki <asitanonegif@gmail.com>
2019-07-22 15:24:55 +0900

    ホームの細かいところ
    フッターのリンク
    ヘッダーのjs

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load',function(){
   $('.header-bottom__right--todolist').hover(function(){
     $(this).addClass('active');
     var hideItem = $('.active').children('.header-hide-item');
@@ -7,4 +7,14 @@ $(document).ready(function(){
     $(this).removeClass('active');
     $(this).children('.header-hide-item').hide();
   });
+
+  $('.header-bottom__right--mypage').hover(function(){
+    $(this).addClass('active');
+    var hideItem = $('.active').children('.header-hide-item')
+    hideItem.show();
+  },function(){
+    // $(this).removeClass('active');
+    // $(this).children('.header-hide-item').hide();
+  });
 });
+
