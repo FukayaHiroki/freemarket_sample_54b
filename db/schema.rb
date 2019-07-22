672aa77577fac46162c618261a5ec2e310f1661c
FukayaHiroki <asitanonegif@gmail.com>
2019-07-22 16:59:00 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_044558) do
-
+ActiveRecord::Schema.define(version: 2019_07_21_052022) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -56,9 +55,9 @@ ActiveRecord::Schema.define(version: 2019_07_21_044558) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "detail", null: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.integer "price"
     t.integer "prefecture_id"
     t.integer "condition_id", null: false
