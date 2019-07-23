9032e899b1c9dbc29a96105c9fbdb3db6e5b3ab5
Tomoki <tech54tu@gmail.com>
2019-07-23 17:42:04 +0900

    孫カテゴリ_その他商品の表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,8 @@ class ProductsController < ApplicationController
     @images   = @product.images
     @comments = @product.comments.includes(:user)
     @comment  = Comment.new
-    @seller_products = Product.where(user_id: @product.user_id).limit(6).order("created_at DESC")
+    @seller_products   = Product.where(user_id:     @product.user_id)    .limit(6).order("created_at DESC")
+    @category_products = Product.where(category_id: @product.category_id).limit(6).order("created_at DESC")
   end
   
   def destroy
