17bf0b70bcdd402331398e1679b1535c41480339
Osatsu-cat <moluca5460@gmail.com>
2019-07-25 21:49:43 +0900

    deviseのアクションに追記

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,12 +11,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    if params['g-recaptcha-response']
-      super
-      SnsCredential.create(sns_params)
+    build_resource(sign_up_params)
+
+    resource.save if params['g-recaptcha-response'] != ""
+    yield resource if block_given?
+    if resource.persisted? 
+      if resource.active_for_authentication? 
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        SnsCredential.create(sns_params)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        SnsCredential.create(sns_params)
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
     else
-      self.resource = resource_class.new
-      respond_with_navigational(resource) { render :new }
+      clean_up_passwords resource
+      set_minimum_password_length
+      render :new
     end
   end
 
