8a94541c4cab1863272556b9b2bd5b5226064059
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-07-25 14:02:41 +0900

    出品機能　画像JS 削除ボタン

--- app/assets/javascripts/new_dropzone.js
+++ app/assets/javascripts/new_dropzone.js
@@ -5,18 +5,16 @@ $(document).on("turbolinks:load", function() {
                     <img class="img1" src=${loadedImageUri}>
                   </figure>
                   <div class="sell-upload-button">
-                    <a href="#">編集</a>
-                    <a href="#">削除</a>
+                    <a>削除</a>
                   </div>
                 </li>`
                 return html
   };
 
   var files_array = [];
-
-  $('#new_item .sell_upload__box').on("change", 'input[type="file"].upload-image', function(e){
+  
+  $('#upload-image').on("change", function(e){
     var file = e.target.files[0];
-    
     files_array.push(file)
     var reader = new FileReader();
 
@@ -28,7 +26,55 @@ $(document).on("turbolinks:load", function() {
     })(file);
     reader.readAsDataURL(file);
 
-    
+    if (files_array.length == 5) {
+      $('.sell_upload__drop').css({
+        display: "none"
+      });
+      $('.sell_upload__box2').css({
+        display: "flex"
+      });
+    }
+  });
+
+  $(".sell_upload__box2").on("change", "#upload-image2", function(e){
+    var file = e.target.files[0];
+    files_array.push(file)
+    var reader = new FileReader();
+
+    reader.onload = (function(file){
+      return function(e){
+        var loadedImageUri = e.target.result
+        $(buildImage(loadedImageUri,)).appendTo(".sell-upload-items2 ul")
+      };
+    })(file);
+    reader.readAsDataURL(file);
+
+    if (files_array.length == 10) {
+      $('.sell_upload__drop2').css({
+        display: "none"
+      });
+    }
+  });
+
+  $(document).on('click', '.sell-upload-button a', function(){
+    var index = $(".sell-upload-button a").index(this);
+    files_array.splice(index - 1, 1);
+    $(this).parent().parent().remove();
+  });
+
+  $('#new_product').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData($(this).get(0));
+    files_array.forEach(function(file){
+      formData.append("images[url][]", file)
+    });
+    $.ajax({
+      url: '/products',
+      type: "POST",
+      data: formData,
+      contentType: false,
+      processData: false,
+    })
   });
 });
 
